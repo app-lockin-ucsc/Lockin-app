14124126328c7112368ad5817456f286e89a9e11
Cainan <cainanenneking2@gmail.com>
2024-12-02 18:33:36 -0800

    test

---
+++ Frontend/components/DailyTimer.tsx
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { ProgressCircle } from 'react-native-svg-circular-progress'; // Example library for circular progress
